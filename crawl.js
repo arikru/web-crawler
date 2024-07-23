0fe01da2a373b9ec0e67843dd9f235f6a09817af
arikru <krumbach.arian@gmail.com>
2024-07-23 22:12:15 +0200

    checkpoint tdd

--- crawl.js
+++ crawl.js
@@ -1,13 +1,16 @@
 function normalizeURL(url) {
-	if (url.trim() === '') {
-		return ''
-	}
+  if (url.trim() === "") {
+    return "";
+  }
 
-	const parsedURL = new URL(url)
-	const normURL = `${parsedURL.hostname}${parsedURL.pathname}`.replace(/\/$/, '')
-	return normURL
+  const parsedURL = new URL(url);
+  const normURL = `${parsedURL.hostname}${parsedURL.pathname}`.replace(
+    /\/$/,
+    "",
+  );
+  return normURL;
 }
 
 module.exports = {
-	normalizeURL
-}
+  normalizeURL,
+};
