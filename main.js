0a26708825d549f9cf4fb55d616fce7b877c0f0e
arikru <krumbach.arian@gmail.com>
2024-07-24 20:25:18 +0200

    main argv added
    main argv added

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { argv } from "node:process";
+
+function main() {
+  if (argv.length === 3) {
+    console.log(`Start crawlin' ${argv[2]} ...`);
+  } else {
+    throw new Error(
+      `Wrong number of args: ${argv.length - 2} provided but 1 expected`,
+    );
+  }
+}
+
+main();
